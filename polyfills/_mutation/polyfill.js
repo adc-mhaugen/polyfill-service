f8a7a3a3b96a689becc9ff2dc3f1a223fc0fa7f9
Jake Champion <me@jakechampion.name>
2017-08-22 14:18:55 +0100

    Ignore unused variable linting rule

--- polyfills/_mutation/polyfill.js
+++ polyfills/_mutation/polyfill.js
@@ -1,5 +1,5 @@
 // http://dom.spec.whatwg.org/#mutation-method-macro
-function _mutation(nodes) {
+function _mutation(nodes) { // eslint-disable-line no-unused-vars
 	if (nodes.length === 1) {
 		return nodes[0] instanceof Node ? nodes[0] : document.createTextNode(nodes[0]+'');
 	}
